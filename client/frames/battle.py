45f9bafe928995777c1bd55b58c34a5b8c114277
meverg <eminvergili96@gmail.com>
2018-12-24 01:00:34 +0300

    Add animation and stuff

--- client/frames/battle.py
+++ client/frames/battle.py
@@ -1,7 +1,8 @@
 import traceback
 import tkinter as tk
 import frames.mainpage as mpage
-import os
+from frames.animatedGif import AnimatedGIF
+
 class Battle(tk.Frame):
   def __init__(self, master,args=None):
     tk.Frame.__init__(self, master)  
@@ -11,31 +12,27 @@ class Battle(tk.Frame):
     self._side = args["side"]
     self._opponentName = args["opponentName"]
     # opponent info
-    # TODO image names
-    tk.Label(self,text="Opponent: "+args["opponentName"]).grid(row = 0, column = 0, columnspan = 12, sticky = 'W')#, sticky = 'S'
+    tk.Label(self,text="Opponent: "+args["opponentName"]).grid(row = 0, column = 0, columnspan = 12, sticky = 'E')#, sticky = 'S'
     self._opponentMonsterNameLabel = tk.Label(self,text="-", width = 10, bg = "blue")
     self._opponentMonsterNameLabel.grid(row = 1, column = 6, columnspan = 2, sticky = 'EN')
-    self._opponentMonsterImage = tk.PhotoImage(file = "./resources/"+args["opponentImage"])
-    self._opponentMonsterImageLabel = tk.Label(self, image = self._opponentMonsterImage, width = 256, bg = "cyan")
-    self._opponentMonsterImageLabel.photo = self._opponentMonsterImage
-    self._opponentMonsterImageLabel.grid(row = 1, column = 8, columnspan = 4, sticky = 'EW')
+    self._opponentMonsterImage = AnimatedGIF(self, "./resources/"+args["opponentImage"])
+    self._opponentMonsterImage.grid(row = 1, column = 8, columnspan = 4, sticky = 'EW')
+    self._opponentMonsterImage.start_animation()
     self._opponentMonsterHPLabel = tk.Label(self,text="-", width = 10, bg = "red")
     self._opponentMonsterHPLabel.grid(row = 5, column = 6, columnspan = 6, sticky = 'E')
     self.updateMonsterInfo("opponent",args["opponentInfo"])
     # monster info
     self._monsterHPLabel = tk.Label(self,text="-", width = 10, bg = "red")
     self._monsterHPLabel.grid(row = 6, column = 0, columnspan = 6, sticky = 'W')
-    self._monsterImage = tk.PhotoImage(file = "./resources/"+args["monsterImage"])
-    self._monsterImageLabel = tk.Label(self, image = self._monsterImage, width = 256, bg = "cyan")
-    self._monsterImageLabel.photo = self._monsterImage
-    self._monsterImageLabel.grid(row = 7, column = 0, columnspan = 4, rowspan = 4, sticky = 'EW')
+    self._monsterImage = AnimatedGIF(self, "./resources/"+args["monsterImage"])
+    self._monsterImage.grid(row = 7, column = 0, columnspan = 4, rowspan = 4, sticky = 'EW')
+    self._monsterImage.start_animation()
     self._monsterNameLabel = tk.Label(self, text="-", width = 10, bg = "blue")
     self._monsterNameLabel.grid(row = 10, column = 4, columnspan = 2, sticky = 'WS')
     self.updateMonsterInfo("my",args["monsterInfo"])
-    # tk.Label(self,text=self._master._username).pack()
     # action is not selected if true, otherwise false
     self._isActive = True
-    self._statusLabelActiveText = "Select an action"
+    self._statusLabelActiveText = "Select an action " + self._master._username
     self._statusLabelWaitingText = "Waiting for your opponent action"
     self._statusLabel = tk.Label(self,text=self._statusLabelActiveText)
     self._statusLabel.grid(row = 12, column = 0, columnspan = 6, sticky = 'EW')
@@ -50,11 +47,13 @@ class Battle(tk.Frame):
     self._thirdActionButton = tk.Button(self, text=actionNames[2], width = 10)
     self._thirdActionButton.bind("<Button-1>", lambda event: self.action("2"))
     self._thirdActionButton.grid(row = 11, column = 4, columnspan = 2)#, sticky = 'S'
-    ## tk.Button(self, text="Return to main page", command=lambda: master.switch_frame(mpage.MainPage)).pack()
-    self._logLabel = tk.Label(self,text="Logs:\n", width = 40, bg = "green", relief = tk.SUNKEN, bd = 5)
-    self._logLabel.grid(row = 0, column = 12, columnspan = 6, rowspan = 11, sticky = 'N')
+    self._logLabel = tk.Label(self,text="Logs:\n", width = 40, bg = "green", relief = tk.SUNKEN, bd = 5, anchor = 'n')
+    self._logLabel.grid(row = 0, column = 12, columnspan = 6, rowspan = 12, sticky = 'NSWE')
+    self._retreatButton = tk.Button(self, text="Return to main page", width = 20, background = "red3")
+    self._retreatButton.bind("<Button-1>", self.backPressed)
+    self._retreatButton.grid(row = 12, column = 16, columnspan = 2, sticky = 'E')
 
-  def backPressed(self):
+  def backPressed(self, event):
     message = "battleLeft;"+self._battleKey+";"+self._side
     self._master.sendToServer(message)
     self._master.switch_frame(mpage.MainPage)
